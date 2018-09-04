6d173d87228766566b5658327152d4199c230577
Benoit Lemoine <blemoine@mnubo.com>
2018-09-04 12:19:45 -0400

    control on magnitude

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,15 @@ import { Point } from 'geojson';
 import { convertToGeoJson, HygProperty } from '../hygdata/hygdata';
 import { parse } from 'papaparse';
 import { isError } from '../utils/validated';
+import { Controls } from '../controls/controls';
 
 export class App extends React.Component<{}, { geoJson: GeoJSON.FeatureCollection<Point, HygProperty> | null }> {
   state = {
     geoJson: null,
+    maxMagnitude: 6,
   };
+
+  private csv: Array<Array<string>> = [];
 
   componentDidMount() {
     fetch('data/hygdata_v3.csv')
@@ -25,8 +29,8 @@ export class App extends React.Component<{}, { geoJson: GeoJSON.FeatureCollectio
           */
           console.error(parsed.errors);
         }
-        const csv = parsed.data;
-        const geoJson = convertToGeoJson(csv, (magnitude: number) => magnitude < 6);
+        this.csv = parsed.data;
+        const geoJson = convertToGeoJson(this.csv, (magnitude: number) => magnitude < this.state.maxMagnitude);
         if (isError(geoJson)) {
           console.error(...geoJson.errors());
         } else {
@@ -35,11 +39,34 @@ export class App extends React.Component<{}, { geoJson: GeoJSON.FeatureCollectio
       });
   }
 
+  private updateMagnitude(maxMagnitude: number) {
+    this.setState((prevState) => ({ ...prevState, maxMagnitude }));
+    const geoJson = convertToGeoJson(this.csv, (magnitude: number) => magnitude < maxMagnitude);
+    if (isError(geoJson)) {
+      console.error(...geoJson.errors());
+    } else {
+      this.setState((prevState) => ({ ...prevState, geoJson }));
+    }
+  }
+
   render() {
     const geoJson = this.state.geoJson;
 
     return (
       <>
+        <div
+          style={{
+            position: 'absolute',
+            width: '100%',
+            minHeight: '50px',
+            backgroundColor: 'rgba(55,55,55,0.8)',
+          }}
+        >
+          <Controls
+            magnitude={this.state.maxMagnitude}
+            magnitudeChange={(magnitude) => this.updateMagnitude(magnitude)}
+          />
+        </div>
         <div className="main-wrapper">
           {geoJson ? <StarMap geoJson={geoJson} width="100vw" height="100vh" /> : 'LOADING...'}
         </div>
---
+++ src/controls/controls.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import './controls.css'
+
+type Props = {
+  magnitude: number;
+  magnitudeChange: (magnitude: number) => void;
+};
+export const Controls = (props: Props) => {
+  return (
+    <div>
+      <div className="form-element">
+        <label>Magnitude</label>
+        <input
+          type="number"
+          value={props.magnitude}
+          onChange={(e) => props.magnitudeChange(parseFloat(e.target.value))}
+          max={6}
+          step={0.25}
+        />
+      </div>
+    </div>
+  );
+};
