e81bb00ed6b9756fae5ca38cff5417e3a77c9ef6
Benoit Lemoine <blemoine@mnubo.com>
2018-09-29 08:24:38 -0400

    feat(tooltip): add some tooltips

--- src/controls/controls.tsx
+++ src/controls/controls.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import Tooltip from 'rc-tooltip';
+import 'rc-tooltip/assets/bootstrap_white.css';
 import './controls.css';
 
 type Props = {
@@ -11,7 +13,20 @@ export const Controls = (props: Props) => {
   return (
     <div className="controls-wrapper">
       <div className="form-element">
-        <label>Magnitude</label>
+        <Tooltip
+          placement="bottom"
+          overlay={
+            <span>
+              The <a href="https://en.wikipedia.org/wiki/Apparent_magnitude">Magnitude</a> is a measurement of the
+              luminosity of stars. The higher the magnitude, the less visible is the star. <br />
+              The stars with magnitude greater than 6 are not visible with naked eyes. In cities, stars with magnitude
+              greater than 3 are often not visible.
+            </span>
+          }
+          destroyTooltipOnHide={true}
+        >
+          <label>Magnitude</label>
+        </Tooltip>
         <input
           type="number"
           value={props.magnitude}
@@ -21,9 +36,24 @@ export const Controls = (props: Props) => {
         />
       </div>
       <div className="form-element">
-        <button onClick={() => props.displayConstellationChange(!props.displayConstellation)}>
-          {props.displayConstellation ? 'Hide Constellations' : 'Show Constellations'}
-        </button>
+        <Tooltip
+          placement="bottom"
+          overlay={
+            <span>
+              <a href="https://en.wikipedia.org/wiki/Constellation">Constellation</a> are often <em>drawn</em> from
+              stars that have no other links than to be in same general direction. Those stars can in reality be really
+              far from each others. <br />
+              So, while staying in the neighbourhood (some light-years) of the solar system, the
+              constellation will appear slightly deformed, the further you move, the less the constellation will make
+              sense. That's why you can hide them if you want to navigate far into the galaxy.
+            </span>
+          }
+          destroyTooltipOnHide={true}
+        >
+          <button onClick={() => props.displayConstellationChange(!props.displayConstellation)}>
+            {props.displayConstellation ? 'Hide Constellations' : 'Show Constellations'}
+          </button>
+        </Tooltip>
       </div>
     </div>
   );
